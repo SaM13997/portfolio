9b8fc93044bf325148e1eec3421d99abe9fd8492
Sarthak <sarthakmalhotra251@gmail.com>
2024-01-11 14:15:31 +0530

    init

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+	return (
+		<main>
+			<div className="flexbox">
+				<div className="flexitem1"></div>
+				<div className="flexitem2"></div>
+			</div>
+		</main>
+	)
+}
